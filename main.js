bcc41ae3a9b199dc46d9bc6f48e88b835de933e8
Borys Lopez <68643829+boryslopez02@users.noreply.github.com>
2024-11-05 11:51:19 -0400

    part 1

---
+++ main.js
@@ -0,0 +1,26 @@
+gsap.from("#img1",{
+    delay: 0.4,
+    opacity: 0,
+    y: 60,
+    duration: 1
+})
+
+gsap.from("#img2",{
+    delay: 0.4,
+    opacity: 0,
+    x: 60,
+    duration: 1
+})
+
+gsap.from("#img3",{
+    delay: 0.4,
+    opacity: 0,
+    y: -60,
+    duration: 1
+})
+
+gsap.from("#main h1",{
+    delay: 0.4,
+    opacity: 0,
+    duration: 1
+})
